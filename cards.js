101b5bc6567a799e483eff55f596dc69b6aeacc8
borjanieto-ctrl <borja_nieto@hotmail.es>
2026-08-08 01:01:02 +0200

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,17 @@
+// ======================================
+// NOX
+// cards.js
+// Gestión de cartas
+// ======================================
+
+function setDeck(deckName){
+
+    Game.deck = deckName;
+
+}
+
+function getDeck(){
+
+    return Game.deck;
+
+}
