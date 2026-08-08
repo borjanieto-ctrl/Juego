2522826152c2dbfd1b1209309787c21271b5514b
borjanieto-ctrl <borja_nieto@hotmail.es>
2026-08-08 08:15:18 +0200

    Create engine.js

---
+++ js/engine.js
@@ -0,0 +1,23 @@
+// ======================================
+// NOX
+// engine.js
+// Estado del juego
+// ======================================
+
+const Game = {
+
+    deck: null,
+
+    cards: [],
+
+    currentCard: null,
+
+    history: [],
+
+    favorites: [],
+
+    difficulty: "normal",
+
+    started: false
+
+};
